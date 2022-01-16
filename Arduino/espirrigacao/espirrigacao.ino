399a67ae479a8a4389d29e07f1e86c73d0b0dc51
João Victor <joaovictor@fitdance.com>
2022-01-16 16:29:12 -0300

    feat: ticker to firebase data sending

--- Arduino/espirrigacao/espirrigacao.ino
+++ Arduino/espirrigacao/espirrigacao.ino
@@ -1,3 +1,4 @@
+// libraries
 #include <stdio.h>
 #include <string.h>
 #include <ESP8266WebServer.h>
@@ -5,68 +6,72 @@
 #include <DNSServer.h>
 #include <WiFiManager.h>
 #include <ESP8266Firebase.h>
+#include <Ticker.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
 
-const char* ssid = "brisa-1397440";  // Enter SSID here
-const char* password = "t1rehfdu";  //Enter Password here
-
-ESP8266WebServer server(80);
-WiFiManager wifiManager;
-
+// definitions
 #define DHTPIN 4
 #define DHTTYPE DHT11
 #define FIREBASE_ID "espwater-25fb5-default-rtdb"
 #define FIREBASE_FINGERPRINT "93 49 BD 13 3F AD AE EB 44 9B DA EA 6E 7F 27 A3 2E D1 73 7B"
+#define PUBLISH_INTERVAL 1000*60*60
 
+// initializations
+ESP8266WebServer server(80);
+WiFiManager wifiManager;
 DHT dht(DHTPIN, DHTTYPE);
 Firebase firebase(FIREBASE_ID, FIREBASE_FINGERPRINT);
+Ticker ticker;
+bool publishNewState = true;
+
+void publish(){
+  publishNewState = true;
+}
+
 
 void setup() 
 {
   Serial.begin(115200);
   Serial.println("BEGIN");
   delay(50);
-  wifiManager.resetSettings();
-  
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-   delay(500);
-  }
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 
   wifiManager.setSaveConfigCallback(saveConfigCallback);
   wifiManager.autoConnect();
   dht.begin();
-  
-  server.on("/dados", handleGetData);
+
+  ticker.attach_ms(PUBLISH_INTERVAL, publish);
 }
 
-float temperature;
-float humidity;
+String firebasePath = "";
  
 void loop() 
 {
   server.handleClient();
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity(); 
+  
+  if (firebasePath.length() > 0 && WiFi.status() == WL_CONNECTED && publishNewState) {
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
 
-  delay(500);
+    firebase.setString(firebasePath+"temperature/", String(temperature));
+    firebase.setString(firebasePath+"humidity/", String(humidity));
+
+    publishNewState = false;
+  }
 }
 
-
-void handleGetData(){
-  server.send(200, "text/plain", String(temperature));
-}
 
 void saveConfigCallback() {
   String ssid = wifiManager.getWiFiSSID();
   String password = wifiManager.getWiFiPass();
   String espIp = WiFi.localIP().toString();
-  String path = "networks/"+ssid+"/";
+  firebasePath = "networks/"+ssid+"/";
 
-  firebase.setString(path+"ip/", espIp);
-  firebase.setString(path+"password/", password);
+  firebase.setString(firebasePath+"ip/", espIp);
+  firebase.setString(firebasePath+"password/", password);
+}
+
+void handleAction(){
+  // switch case action name
 }
   
